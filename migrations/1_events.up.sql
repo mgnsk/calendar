5205938b565e98fbfc49faf6f1c8285b0ca87bb9
Magnus Kokk <magnus@kokk.eu>
2025-01-28 19:43:56 +0200

    Clean up migrations

--- migrations/1_events.up.sql
+++ migrations/1_events.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `events` (
   `id` bigint unsigned NOT NULL,
-  `unix_timestamp` bigint unsigned NOT NULL,
+  `unix_timestamp` bigint NOT NULL,
   `human_timestamp` text NOT NULL,
   `title` text NOT NULL,
   `content` text NOT NULL,
--- migrations/2_events.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `events` (
-  `id` bigint unsigned NOT NULL,
-  `unix_timestamp` bigint unsigned NOT NULL,
-  `human_timestamp` text NOT NULL,
-  `title` text NOT NULL,
-  `content` text NOT NULL,
-  PRIMARY KEY (`id`)
-);
