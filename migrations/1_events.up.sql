0877939c95fa20002449c5c86bc48d623b170573
Magnus Kokk <magnus@kokk.eu>
2025-02-24 17:22:28 +0200

    Fix venue coordinates datatype in database

--- migrations/1_events.up.sql
+++ migrations/1_events.up.sql
@@ -6,8 +6,8 @@ CREATE TABLE `events` (
   `description` text NOT NULL,
   `url` text NOT NULL,
   `location` text NOT NULL,
-  `latitude` text NOT NULL,
-  `longitude` text NOT NULL,
+  `latitude` real NOT NULL,
+  `longitude` real NOT NULL,
   `is_draft` tinyint NOT NULL,
   `user_id` bigint unsigned NOT NULL
 );
