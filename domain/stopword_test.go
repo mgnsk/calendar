778789b5fca0f35ffc64091085f6df287f91935c
Magnus Kokk <magnus@kokk.eu>
2025-08-15 17:24:23 +0300

    Add test for stopwords domain model

---
+++ domain/stopword_test.go
@@ -0,0 +1,33 @@
+package domain_test
+
+import (
+	"github.com/mgnsk/calendar/domain"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("creating a stop word list", func() {
+	Specify("words are trimmed", func() {
+		words := domain.NewStopWordList(" a ")
+
+		Expect(words).To(HaveExactElements(
+			HaveField("Word", "a"),
+		))
+	})
+
+	Specify("words are lowercased", func() {
+		words := domain.NewStopWordList("A")
+
+		Expect(words).To(HaveExactElements(
+			HaveField("Word", "a"),
+		))
+	})
+
+	Specify("duplicate words are removed", func() {
+		words := domain.NewStopWordList("A", "a")
+
+		Expect(words).To(HaveExactElements(
+			HaveField("Word", "a"),
+		))
+	})
+})
