a88351280cd43e8cd3a2f9aba83539d825c73499
Magnus Kokk <magnus@kokk.eu>
2026-03-06 17:15:18 +0200

    Use webcal link for google

--- html/components/UserNav.go
+++ html/components/UserNav.go
@@ -16,8 +16,9 @@ func UserNav(user *domain.User, children Node) Node {
 				A(Class("inline-block p-2"), Title("RSS feed"), Href("/feed"), rssIcon()),
 				A(Class("inline-block p-2"), Title("iCal URL"), ID("ical-link"), calendarIcon()),
 				A(Class("inline-block p-2"), Title("Add to Google Calendar"), ID("google-calendar-link"), Target("_blank"), calendarIcon()),
-				Script(Raw(`document.getElementById("ical-link").setAttribute("href", "webcal://" + window.location.host + "/calendar.ics")`)),
-				Script(Raw(`document.getElementById("google-calendar-link").setAttribute("href", "https://calendar.google.com/calendar/render?cid=" + "http://" + window.location.host + "/calendar.ics")`)),
+				Script(Raw(`window.webcalURL = "webcal://" + window.location.host + "/calendar.ics"`)),
+				Script(Raw(`document.getElementById("ical-link").setAttribute("href", window.webcalURL)`)),
+				Script(Raw(`document.getElementById("google-calendar-link").setAttribute("href", "https://calendar.google.com/calendar/render?cid=" + window.webcalURL)`)),
 			),
 
 			Iff(user != nil, func() Node {
