22dec010ded86a7974324929418904345a2d7e59
Magnus Kokk <magnus@kokk.eu>
2026-06-13 14:00:42 +0300

    Display event date in user timezone, show event-local time on hover

--- html/event.go
+++ html/event.go
@@ -157,9 +157,14 @@ func eventDay(ev *domain.Event) Node {
 }
 
 func eventDate(ev *domain.Event) Node {
+	id := fmt.Sprintf("event-date-%d", ev.ID)
+	startAt := ev.StartAt.Format(time.RFC1123Z)
+
 	return Group{
-		H2(Class("block mt-2 uppercase tracking-wide text-sm text-amber-600 font-semibold"),
-			Text(ev.GetDateString()),
+		H2(ID(id), Class("block mt-2 uppercase tracking-wide text-sm text-amber-600 font-semibold"),
+			Text(startAt),
+			Title(startAt),
 		),
+		Script(Rawf(`var el = document.getElementById(%q); el.textContent = new Date(el.textContent).toLocaleString(navigator.language, {dateStyle: 'long', timeStyle: 'short'})`, id)),
 	}
 }
